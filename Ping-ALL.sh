2c299e39354dcc7a50f592f51017f0cf394a5752
Brian Bauska <41387907+bbauska@users.noreply.github.com>
2020-08-16 18:13:50 -0700

    Add files via upload

---
+++ Ping-ALL.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#ping -c 7 10.51.10.110 >> $_file
+_now=$(date +"%Y_%m_%d")
+#_file="~/Desktop/ping_ALL_$_now.txt"
+#>> _file
+#echo "Starting ping on Net 8: $_file" >> $_file
+#_file_r8="~/Desktop/ping_Net8_$_now.txt"
+#>> _file_r8
+ping -c 7 10.51.10.80 >> ~/Desktop/Ping-Net8-Master_$_now.txt
+ping -c 7 10.51.10.81 >> ~/Desktop/Ping-Net8-1_$_now.txt
+ping -c 7 10.51.10.82 >> ~/Desktop/Ping-Net8-2_$_now.txt
+ping -c 7 10.51.10.83 >> ~/Desktop/Ping-Net8-3_$_now.txt
+ping -c 7 10.51.10.84 >> ~/Desktop/Ping-Net8-4_$_now.txt
+ping -c 7 10.51.10.85 >> ~/Desktop/Ping-Net8-5_$_now.txt
+ping -c 7 10.51.10.86 >> ~/Desktop/Ping-Net8-2a_$_now.txt
+ping -c 7 10.51.10.87 >> ~/Desktop/Ping-Net8-3a_$_now.txt
+ping -c 7 10.51.10.88 >> ~/Desktop/Ping-Net8-4a_$_now.txt
+ping -c 7 10.51.10.89 >> ~/Desktop/Ping-Net8-5a_$_now.txt
+#_file_r9="~/Desktop/ping_Net9_$_now.txt"
+#>>$_file_r9
+ping -c 7 10.51.10.90 >> ~/Desktop/Ping-Net9-Master_$_now.txt
+ping -c 7 10.51.10.91 >> ~/Desktop/Ping-Net9-1_$_now.txt
+ping -c 7 10.51.10.92 >> ~/Desktop/Ping-Net9-2_$_now.txt
+ping -c 7 10.51.10.93 >> ~/Desktop/Ping-Net9-3_$_now.txt
+ping -c 7 10.51.10.94 >> ~/Desktop/Ping-Net9-4_$_now.txt
+ping -c 7 10.51.10.95 >> ~/Desktop/Ping-Net9-5_$_now.txt
+ping -c 7 10.51.10.96 >> ~/Desktop/Ping-Net9-2a_$_now.txt
+ping -c 7 10.51.10.97 >> ~/Desktop/Ping-Net9-3a_$_now.txt
+ping -c 7 10.51.10.98 >> ~/Desktop/Ping-Net9-4a_$_now.txt
+ping -c 7 10.51.10.99 >> ~/Desktop/Ping-Net9-5a_$_now.txt
+#_file_r10="~/Desktop/ping_Net10_$_now.txt"
+#>>$_file_r10
+ping -c 7 10.51.10.100 >> ~/Desktop/Ping-Net10-Master_$_now.txt
+ping -c 7 10.51.10.101 >> ~/Desktop/Ping-Net10-1_$_now.txt
+ping -c 7 10.51.10.102 >> ~/Desktop/Ping-Net10-2_$_now.txt
+ping -c 7 10.51.10.103 >> ~/Desktop/Ping-Net10-3_$_now.txt
+ping -c 7 10.51.10.104 >> ~/Desktop/Ping-Net10-4_$_now.txt
+ping -c 7 10.51.10.105 >> ~/Desktop/Ping-Net10-5_$_now.txt
+ping -c 7 10.51.10.106 >> ~/Desktop/Ping-Net10-2a_$_now.txt
+ping -c 7 10.51.10.107 >> ~/Desktop/Ping-Net10-3a_$_now.txt
+ping -c 7 10.51.10.108 >> ~/Desktop/Ping-Net10-4a_$_now.txt
+ping -c 7 10.51.10.109 >> ~/Desktop/Ping-Net10-5a_$_now.txt
+#_file_r11="~/Desktop/ping_Net11_$_now.txt"
+#>>$_file_r11
+ping -c 7 10.51.10.110 >> ~/Desktop/Ping-Net11-Master_$_now.txt
+ping -c 7 10.51.10.111 >> ~/Desktop/Ping-Net11-1_$_now.txt
+ping -c 7 10.51.10.112 >> ~/Desktop/Ping-Net11-2_$_now.txt
+ping -c 7 10.51.10.113 >> ~/Desktop/Ping-Net11-3_$_now.txt
+ping -c 7 10.51.10.114 >> ~/Desktop/Ping-Net11-4_$_now.txt
+ping -c 7 10.51.10.115 >> ~/Desktop/Ping-Net11-5_$_now.txt
+ping -c 7 10.51.10.116 >> ~/Desktop/Ping-Net11-2a_$_now.txt
+ping -c 7 10.51.10.117 >> ~/Desktop/Ping-Net11-3a_$_now.txt
+ping -c 7 10.51.10.118 >> ~/Desktop/Ping-Net11-4a_$_now.txt
+ping -c 7 10.51.10.119 >> ~/Desktop/Ping-Net11-5a_$_now.txt
+exit
